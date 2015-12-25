f220f5b1cb897e326426f5d845422fdaaa66d08a
Cesare de Cal <hi@cesare.io>
2015-12-25 12:43:17 +0100

    Working prototype

--- talkingPlant/talkingPlant.ino
+++ talkingPlant/talkingPlant.ino
@@ -1,7 +1,20 @@
 /* Talking plant */
 
+const int moistureSensor = A0;
+const int led = 13;
+
 void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(A0, INPUT);
 }
 
 void loop() {
+  int moistureState = analogRead(A0);
+  if (moistureState < 550) {
+     Serial.println(moistureState);
+     digitalWrite(led, HIGH);
+  } else {
+     digitalWrite(led, LOW);
+  }
 }
