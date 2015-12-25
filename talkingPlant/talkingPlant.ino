b4a067dd71f067d704a74574eee26c65a304a4b8
Cesare de Cal <hi@cesare.io>
2015-12-25 14:42:11 +0100

    Added LCD support

--- talkingPlant/talkingPlant.ino
+++ talkingPlant/talkingPlant.ino
@@ -1,9 +1,17 @@
 /* Talking plant */
+#include <LiquidCrystal.h>
 
 const int moistureSensor = A0;
-const int led = 13;
+const int led = 6;
+LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+int backLight = 13;    // pin 13 will control the backlight
 
 void setup() {
+  pinMode(backLight, OUTPUT);
+  analogWrite(backLight, 150);
+  lcd.begin(16,2);                    // columns, rows. size of display
+  lcd.clear();                        // clear the screen
+  
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   pinMode(A0, INPUT);
@@ -14,7 +22,23 @@ void loop() {
   if (moistureState < 550) {
      Serial.println(moistureState);
      digitalWrite(led, HIGH);
+     int random = rand() + 4;
+     lcd.print("Ho sete!");
+     delay(300);
+     lcd.clear();
+     delay(300);
+     lcd.print("Acqua!");
+     delay(300);
+     lcd.clear();
+     delay(300);
+     lcd.print("Ti prego!");
+     delay(300);
+     lcd.clear();
+     delay(300);
   } else {
      digitalWrite(led, LOW);
+     lcd.clear();
+     delay(300);
+     lcd.print("Grazie salvatore!");
   }
 }
